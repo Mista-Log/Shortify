3103de0159b02a2f47023cb73ce5a2995abab464
Mista Log <oloyedeoibrahimsmile@gmail.com>
2024-12-26 11:45:06 +0100

    changes

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)m_1yqb!&gb%_8q)xk!vljcxdel0x&+%di*&10t+j(88x0vh(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
